59a77a51a6f5acbb10d7ed99e842e890435d929c
Lalla Khadija Mendili <mendilikhadija3@gmail.com>
2024-06-25 09:21:13 -0400

    Initial Commit

---
+++ Sustainify/SustainifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SustainifyApp.swift
+//  Sustainify
+//
+//  Created by Lalla Khadija Mendili on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SustainifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
